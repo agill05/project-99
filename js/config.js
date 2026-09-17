5cf968bdd0f10f6ce2a774a93886aae4e44e0ca6
agill05 <agilabddul30@gmail.com>
2026-09-17 22:50:28 +0800

    update

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ export const DB_NAME = 'ELKPD_STEAM_DB';
 export const DB_VERSION = 1;
 
 export const GAS_API_URL =
-  'https://script.google.com/macros/s/AKfycbwEru2KE5XRo3LAszo2bgXl2vKhRv8xlAgKfaSibs1aM_PSKqvNJgeI73KCMNnMEUoqkg/exec';
+  'https://script.google.com/macros/s/AKfycbzo4EuwAmoMGtwfgl2ytphYBmvWKjitNh7r0c6hnubJnqYKDhCOOz4qEm6HXClrfFHckw/exec';
 
 export const CACHE_KEY = 'ELKPD_STEAM_CACHE_DATA_V3';
 
